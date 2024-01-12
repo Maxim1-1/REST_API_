f5ffa138cbdd997186bc4929f84ef70d77c3502f
Maxim1-1 <kick52@yadnex.ru>
2024-01-12 22:07:26 +0300

    add conf

--- src/main/java/com/Maxim/controller/UsersServlet.java
+++ src/main/java/com/Maxim/controller/UsersServlet.java
@@ -2,7 +2,11 @@ package com.Maxim.controller;
 
 
 import com.Maxim.model.User;
+import com.Maxim.model.User;
 import com.Maxim.service.UserService;
+import com.Maxim.service.UserService;
+import com.Maxim.view.UserView;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,6 +14,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -18,30 +23,59 @@ import java.util.List;
 @WebServlet("/users")
 public class UsersServlet extends HttpServlet {
 
+    private UserService userService = new UserService();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> events = userService.getAllUsers();
 
-        UserService userService = new UserService();
-        List<User> users = userService.getAllUsers();
-
+        resp.setContentType("application/json");
+        UserView eventView = new UserView();
+        eventView.getUsers(events,resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line.strip());
+        }
+        String json = sb.toString();
 
+        System.out.println(json);
+        User event = new ObjectMapper().readerFor( User.class).readValue(json);
 
-            Gson gson = new Gson();
-
-            UserService userService = new UserService();
-            List<User> users2 = userService.getAllUsers();
-
-            resp.setContentType("application/json");
-
-            PrintWriter messageWriter = resp.getWriter();
-            messageWriter.print(gson.toJson(users2));
-
-
+        userService.saveUser(event);
     }
 
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String event_id = req.getParameter("event_id");
+        if(event_id!=null) {
+            userService.deleteUserById(Integer.valueOf(event_id));
+        } else {
+            resp.sendError(400);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line.strip());
+        }
+        String json = sb.toString();
+        User event = new ObjectMapper().readerFor(User.class).readValue(json);
+
+        if (userService.getUserById(event.getId())!=null) {
+            userService.updateUserById(event);
+        } else {
+            resp.sendError(400,"File not exist");
+        }
+    }
 
 }
